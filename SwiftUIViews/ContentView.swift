75f6d9715183f084f4eb29271e23b0b97d8af061
Inozemtsev Vladimir <vinozemtsev@ozon.ru>
2020-07-24 10:08:34 +0300

    Initial commit

--- SwiftUIViews/ContentView.swift
+++ SwiftUIViews/ContentView.swift
@@ -9,13 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let tutors: [Tutor]
+    
     var body: some View {
-        Text("Hello, World!")
+        List(tutors) {
+            Cell(tutor: $0)
+        }
+    }
+}
+
+struct Cell: View {
+    
+    let tutor: Tutor
+    
+    var body: some View {
+        HStack {
+            Image(tutor.imageName)
+                .cornerRadius(40)
+            VStack(alignment: .leading, spacing: 4) {
+                Text(tutor.name)
+                    .font(.headline)
+                Text(tutor.headline)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(tutors: testData)
     }
 }
